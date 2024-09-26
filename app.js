8374c5b902b0fa41a9d970731137378043c0f890
mohsinalimughal <mohsinalimughal112233445566@gmail.com>
2024-09-26 02:57:37 -0700

    ok

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var head = document.querySelector("#head")
 var tale = document.querySelector("#tale")
 var result = document.querySelector("#result")
 var img = document.querySelector("img")
+var score1 = document.querySelector("#score")
+var count1 = document.querySelector("#count")
 
+score = 0
+count = 0
 
 function clickHead(){
        userChoise = 1
@@ -10,11 +14,18 @@ function clickHead(){
        console.log(compChoise)
        if(compChoise===1){
          img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST-PtiC-NYMo3H-AEjSp6UvNbL_qfXjK68GQ&s"
-         result.innerHTML = `You won`
+         result.innerHTML = `You won The Toss`
+         count++
+         score++
+         count1.innerHTML=`Attempt Count : ${count}`
+         score1.innerHTML=`Score : ${score}`
         }else{
             
             img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREDHkvxtlR3dK9-_bdve9QVxiKwITtBu_beQ&s"
-            result.innerHTML = `You loss`
+            result.innerHTML = `You loss The Toss `
+            count++
+        count1.innerHTML=`Attempt Count : ${count}`
+         score1.innerHTML=`Score : ${score}`
        }
 }
 function clickTall(){
@@ -23,10 +34,17 @@ function clickTall(){
        console.log(compChoise)
        if(compChoise===1){
          img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST-PtiC-NYMo3H-AEjSp6UvNbL_qfXjK68GQ&s"
-         result.innerHTML = `You loss`
+         result.innerHTML = `You loss The Toss`
+         count++
+                  count1.innerHTML=`Attempt Count : ${count}`
+         score1.innerHTML=`Score : ${score}`
         }else{
             
             img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREDHkvxtlR3dK9-_bdve9QVxiKwITtBu_beQ&s"
-            result.innerHTML = `You won`
+            result.innerHTML = `You won The Toss`
+            count++
+            score++
+         count1.innerHTML=`Attempt Count : ${count}`
+         score1.innerHTML=`Score : ${score}`
        }
 }
